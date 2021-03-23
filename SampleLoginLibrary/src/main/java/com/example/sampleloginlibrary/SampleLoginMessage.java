19c57b3c8f31f9d04ece9af63bacefd4f1d374db
pravallikab <pravallikab.in@mouritech.com>
2021-03-23 19:23:11 +0530

    Initial Commit

---
+++ SampleLoginLibrary/src/main/java/com/example/sampleloginlibrary/SampleLoginMessage.java
@@ -0,0 +1,20 @@
+package com.example.sampleloginlibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by Pravallika on 23-03-2021.
+ * Updated by Pravallika on 23-03-2021.
+ * <p>
+ * <p>
+ * <p>
+ * Copyright (c) 2021 MouriTech. All rights reserved.
+ */
+public class SampleLoginMessage {
+    public static void s(Context c, String message){
+
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+
+    }
+}
---
+++ app/src/main/java/com/example/sampleloginexample/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.sampleloginexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.sampleloginlibrary.SampleLoginMessage;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
